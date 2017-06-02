c0d5fa838d1ca107d06ce83dd622957aa9b1a8a1
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-02 19:00:39 +0800

    dynamic array

--- Array-Based Sequence/DynamicArray_01.py
+++ Array-Based Sequence/DynamicArray_01.py
@@ -6,9 +6,12 @@ for k in range(20):
     a = len(data)               # number of elements
     b = sys.getsizeof(data)     # actual sizes in bytes
     print("Length: {0:3d}; Size in bytes: {1:4d}".format(a, b))
-    print(id(data))
-
     data.append(None)           # increase size by None
-del data
 
+
+for i in range(21):
+    a = len(data)
+    b = sys.getsizeof(data)
+    print("Length: {0:3d}; Size in bytes: {1:4d}".format(a, b))
+    data.pop()
 
