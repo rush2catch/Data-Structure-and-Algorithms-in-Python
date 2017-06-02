dcdddaafa3cd35a5a3f392e05c605c27bdac2351
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-02 17:33:13 +0800

    compact_array

--- Array-Based Sequence/compact_array.py
+++ Array-Based Sequence/compact_array.py
@@ -1,11 +1,16 @@
+import sys
 from array import array
 
-compact_primes = array('i', [2, 3, 5, 7])
+compact_primes = array('i', [2, 3, 5, 7, 11, 13])
 print(id(compact_primes))
+print(len(compact_primes))
+print(sys.getsizeof(compact_primes))
 for i in range(len(compact_primes)):
     print(compact_primes[i], id(compact_primes[i]))
 
-refer_primes = [2, 3, 5, 7]
+refer_primes = [2, 3, 5, 7, 11, 13]
 print(id(refer_primes))
+print(len(refer_primes))
+print(sys.getsizeof(refer_primes))
 for i in range(len(refer_primes)):
     print(refer_primes[i], id(refer_primes[i]))
