04c9505b08d6dc072596f849967b7adbf0b06cd5
Yuan Jiang <jiangyuan1988@gmail.com>
2017-06-05 19:55:18 +0800

    array

---
+++ Array-Based Sequence/time_analysis.py
@@ -0,0 +1,12 @@
+from time import time
+
+def compute_average(n):
+    data = []
+    start = time()
+    for k in range(n):
+        data.append(None)
+    end = time()
+    return (end - start)
+
+print(compute_average(10))
+print(compute_average(1000000))
